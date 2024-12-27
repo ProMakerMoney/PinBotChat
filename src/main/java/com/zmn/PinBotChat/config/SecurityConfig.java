2c9f6eac6c88aaf39d590a363378fbc5803c8c0c
PinBot <lll24la@gmail.com>
2024-12-27 22:30:22 +0300

    Работает чат и отправка файлов на сервер

--- src/main/java/com/zmn/PinBotChat/config/SecurityConfig.java
+++ src/main/java/com/zmn/PinBotChat/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
