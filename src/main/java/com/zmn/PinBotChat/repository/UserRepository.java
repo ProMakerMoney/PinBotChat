f67d8d92772bfcb5d8b4ed4de73ceba8581e5c0f
PinBot <lll24la@gmail.com>
2025-01-19 18:54:17 +0300

    19.01 - работает

--- src/main/java/com/zmn/PinBotChat/repository/UserRepository.java
+++ src/main/java/com/zmn/PinBotChat/repository/UserRepository.java
@@ -5,6 +5,7 @@ package com.zmn.PinBotChat.repository;
 import com.zmn.PinBotChat.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
